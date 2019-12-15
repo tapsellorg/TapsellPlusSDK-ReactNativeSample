7bfa7722ab6c7876afc2994e73f0e9dce81d3325
behdad222 <behdad.222@gmail.com>
2019-12-15 14:48:22 +0330

    add native

--- app/screen/NativeScreen.js
+++ app/screen/NativeScreen.js
@@ -1,23 +1,53 @@
 import React from 'react';
-import {StyleSheet, View, TouchableOpacity, Text} from 'react-native';
+import {ToastAndroid, StyleSheet} from 'react-native';
+import NativeAd from '../component/NativeAd';
+
+import TapsellPlus from 'react-native-tapsell-plus-sdk';
+import {ZONE_IDS} from '../Constants';
 
 export default class Native extends React.Component {
   static navigationOptions = {
     title: 'Native',
   };
 
-  render() {
-    return (
-      <View>
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>Request</Text>
-        </TouchableOpacity>
+  constructor() {
+    super();
 
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>Show</Text>
-        </TouchableOpacity>
-      </View>
+    this.state = {
+      ad: {
+        ad_id: '',
+        zone_id: '',
+        title: '',
+        description: '',
+        call_to_action_text: '',
+        icon_url: '',
+        portrait_static_image_url: '',
+        landscape_static_image_url: '',
+        error_message: '',
+      },
+    };
+
+    TapsellPlus.requestNative(
+      ZONE_IDS.NATIVE,
+      (adData, onAdClicked) => {
+        this.setState({
+          clickListener: onAdClicked,
+          ad: adData,
+        });
+        ToastAndroid.show('ad is ready', ToastAndroid.SHORT);
+      },
+      error => {
+        ToastAndroid.show('ERROR\n' + error, ToastAndroid.SHORT);
+      },
     );
+  }
+
+  clickFunc = () => {
+    TapsellPlus.nativeAdClicked(ZONE_IDS.NATIVE, this.state.ad.ad_id);
+  };
+
+  render() {
+    return <NativeAd ad={this.state.ad} onNativeAdClicked={this.clickFunc} />;
   }
 }
 
